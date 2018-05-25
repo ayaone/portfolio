9eb33fe02df60f8818fe1cc0026632b713ee4eff
Arek Bochenski <bochenski@gmail.com>
2018-05-25 08:52:45 +0700

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="/css/app.css" rel="stylesheet"/>
+        <title>Arek Bochenski</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+
+    </head>
+    <body>
+        <header class="header">
+            <h1 class="header__text">Bochenski</h1>
+            <ul class="header__menu">
+                <li class="header__menu--item">Home</li>
+                <li class="header__menu--item">Portfolio</li>
+                <li class="header__menu--item">Contact</li>
+                <li class="header__menu--item">About</li>
+            </ul>
+        </header>
+        <script src="/js/app.js"></script>
+    </body>
+</html>
